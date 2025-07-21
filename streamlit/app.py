746ac46ed6cbe2ba3d7e0af296ddd568404064dd
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-21 11:39:15 +0100

    add custom navigation bar

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,3 +4,6 @@ st.set_page_config(layout="wide")
 
 
 st.title("Suggested Actions")
+
+st.sidebar.page_link("app.py", label="Dashboard")
+st.sidebar.page_link("pages/1_Indicators.py", label="Indicators")
