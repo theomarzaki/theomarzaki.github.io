69bb987dc138a1e798b0eba42c02b5e1ea9edf02
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-21 12:08:43 +0100

    change render sidebar

--- streamlit/ui/sidebar.py
+++ streamlit/ui/sidebar.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def render_sidebar():
     st.sidebar.page_link("app.py", label="Home")
-    st.sidebar.page_link("links/1_Indicators.py", label="Indicators")
-    st.sidebar.page_link("links/2_Volume.py", label="Volume")
-    st.sidebar.page_link("links/3_Price_Predictions.py", label="Price Predictions")
-    st.sidebar.page_link("links/4_Risk_Modelling.py", label="Risk Modelling")
+    st.sidebar.page_link("pages/1_Indicators.py", label="Indicators")
+    st.sidebar.page_link("pages/2_Volume.py", label="Volume")
+    st.sidebar.page_link("pages/3_Price_Predictions.py", label="Price Predictions")
+    st.sidebar.page_link("pages/4_Risk_Modelling.py", label="Risk Modelling")
