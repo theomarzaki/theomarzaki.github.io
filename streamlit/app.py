6a5416d77d2caea561afd2992f308e08d870bad3
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-21 12:13:10 +0100

    combine the page configs

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,5 +2,4 @@ import streamlit as st
 from ui.sidebar import render_sidebar
 
 render_sidebar()
-st.set_page_config(page_title="Home")
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Home")
