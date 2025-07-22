5f467e39577c53979f0d8f2b4e1bcce83be9e394
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-22 12:11:19 +0100

    parse the accuracy number itself

--- streamlit/pages/3_Price_Predictions.py
+++ streamlit/pages/3_Price_Predictions.py
@@ -44,7 +44,7 @@ def predict_prices():
 
 def getAccuracy():
     data = pd.read_csv('price_prediction/results/testing_results.csv')
-    return data['R2']
+    return data['R2'].value
 
 
 st.title("Model Predictions - 1 Week Ahead")
