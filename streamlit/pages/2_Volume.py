7a7ae4dffb524af6c1b6a7e5e8ce3c1ddf8360be
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-22 13:51:47 +0100

    fix different subfigs in volume

--- streamlit/pages/2_Volume.py
+++ streamlit/pages/2_Volume.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
-from plotly.subplots import make_subplots
 from ui.sidebar import render_sidebar
 
 render_sidebar()
@@ -66,28 +65,35 @@ if "Price vs Volume" in subpage:
 
     st.plotly_chart(fig, use_container_width=True)
 
-fig = go.Figure()
-if "Volume" in subpage:
-    fig.add_trace(go.Bar(x=df["Date"], y=df["Volume"], name="Volume"))
+if "Volume" in subpage or "Moving Average" in subpage:
+    fig = go.Figure()
+    if "Volume" in subpage:
+        fig.add_trace(go.Bar(x=df["Date"], y=df["Volume"], name="Volume"))
 
-if "Moving Average" in subpage:
-    df["vol_ma_7"] = df["Volume"].rolling(window=7).mean()
-    # Volume + MA
-    fig.add_trace(go.Scatter(x=df["Date"], y=df["vol_ma_7"], name="7D MA", line=dict(color="orange")))
+    if "Moving Average" in subpage:
+        df["vol_ma_7"] = df["Volume"].rolling(window=7).mean()
+        fig.add_trace(go.Scatter(x=df["Date"], y=df["vol_ma_7"], name="7D MA", line=dict(color="orange")))
+    st.plotly_chart(fig, use_container_width=True)
 
-if "Ratio" in subpage:
-    df["vol_ratio"] = df["Volume"] / df["vol_ma_7"]
-    fig.add_trace(go.Scatter(x=df["Date"], y=df["vol_ratio"], name="Volume Ratio", line=dict(color="purple", dash="dot")))
-
-if "OBV" in subpage:
-    # OBV and Volume Ratio
-    fig.add_trace(go.Scatter(x=df["Date"], y=df["OBV"], name="OBV", line=dict(color="green")))
+    fig.update_layout(
+        title="BTC Volume Analytics",
+        height=600,
+        showlegend=True
+    )
 
 
-fig.update_layout(
-    title="BTC Volume Analytics",
-    height=600,
-    showlegend=True
-)
+if "Volume Ratio" in subpage or "OBV" in subpage:
+    fig = go.Figure()
+    if "Volume Ratio" in subpage:
+        df["vol_ratio"] = df["Volume"] / df["vol_ma_7"]
+        fig.add_trace(go.Scatter(x=df["Date"], y=df["vol_ratio"], name="Volume Ratio", line=dict(color="purple", dash="dot")))
 
-st.plotly_chart(fig, use_container_width=True)
+    if "OBV" in subpage:
+        fig.add_trace(go.Scatter(x=df["Date"], y=df["OBV"], name="OBV", line=dict(color="green")))
+    st.plotly_chart(fig, use_container_width=True)
+
+    fig.update_layout(
+        title="BTC Volume Analytics",
+        height=600,
+        showlegend=True
+    )
