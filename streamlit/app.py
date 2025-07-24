bd1c634f1b3fab148fe28400adb6c0dc281f5c1a
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-24 14:20:47 +0100

    see which is being passed

--- streamlit/app.py
+++ streamlit/app.py
@@ -43,8 +43,9 @@ start_of_week_ahead = (current_time - timedelta(days=1)).replace(hour=0, minute=
 data = data.drop_duplicates(subset=['Date'])
 valid_dates = data.loc[(data.Date >= start_of_week_ahead)].Date
 selected_date = st.selectbox("Choose a date", options=valid_dates[::-1])
-snapshot = data[(data['Date'] == selected_date)].iloc[0]
-
+st.markdown(selected_date)
+snapshot = df[df['Date'] == selected_date].iloc[0]
+st.markdown(snapshot)
 
 technical_indicators = getTechnicalIndicatorsFromDate(snapshot)
 
