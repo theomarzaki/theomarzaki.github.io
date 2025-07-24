aac45935ad83a30693155fb74ff7362bdb49d204
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-24 14:15:23 +0100

    convert snapshot to dict

--- streamlit/processing/technical_indicators.py
+++ streamlit/processing/technical_indicators.py
@@ -7,14 +7,14 @@
 
 
 def getTechnicalIndicatorsFromDate(snapshot):
-    snapshot = df.loc[selected_date]
-    kpis = {
-        "RSI": (snapshot['RSI_14'], "PlaceHolder"),
-        "SMA": (snapshot['SMA_20'], "PlaceHolder"),
-        "EMA": (snapshot['EMA_20'], "PlaceHolder"),
-        "MACD": (snapshot['MACD_12_26'], "PlaceHolder"),
+    # kpis = {
+    #     "RSI": (snapshot['RSI_14'], "PlaceHolder"),
+    #     "SMA": (snapshot['SMA_20'], "PlaceHolder"),
+    #     "EMA": (snapshot['EMA_20'], "PlaceHolder"),
+    #     "MACD": (snapshot['MACD_12_26'], "PlaceHolder"),
+    #
+    #
+    # }
 
-
-    }
-
-    return kpis
+    return snapshot.to_dict()
+    # return kpis
