192a657b609773ec6c0268b31c73fd8f69239344
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-25 13:33:50 +0100

    add btc trading agent page

---
+++ streamlit/pages/5_Trader.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from ui.sidebar import render_sidebar
+
+render_sidebar()
+
+st.set_page_config(layout="wide")
+
+
+st.title("BTC Trading Agent")
+
+st.info("Coming Soon...")
