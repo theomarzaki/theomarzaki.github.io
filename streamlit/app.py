5682c8fbe6a68873f57c241e41454ac49206e43e
theomarzaki <omar.nassef@kcl.ac.uk>
2025-07-25 14:07:38 +0100

    let date selection be in the sidebar

--- streamlit/app.py
+++ streamlit/app.py
@@ -35,7 +35,7 @@ data = data.drop_duplicates(subset=['Date'])
 data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d')
 data["OBV_diff"] = data["OBV"].diff()
 valid_dates = data.loc[(data.Date >= start_of_week_ahead)].Date
-selected_date = st.selectbox("Choose a date:", options=valid_dates[::-1])
+selected_date = st.sidebar.selectbox("Choose a date:", options=valid_dates[::-1])
 st.markdown("**Only affects technical and market indicators.*")
 snapshot = data[data['Date'] == selected_date].iloc[-1]
 if snapshot.empty:
