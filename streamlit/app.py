b3312644890aaa8f80c35a9b46a52c9c4fc98acc
theomarzaki <omar.nassef@kcl.ac.uk>
2025-08-03 14:28:29 +0100

    remove hyperlink

--- streamlit/app.py
+++ streamlit/app.py
@@ -53,7 +53,7 @@ with col2:
         </div>
     """, unsafe_allow_html=True)
 
-st.markdown("[See more at Backtesting page](#Trader)")
+st.markdown("See more metrics at Asset Trader page")
 
 st.divider()
 
